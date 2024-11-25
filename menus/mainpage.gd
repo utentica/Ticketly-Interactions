73e00ca5117ee28e7a1bb71c844e63a0e7d6530d
utentica <rodrigogmrpreto@gmail.com>
2024-11-25 18:06:33 +0000

    final talvez

--- menus/mainpage.gd
+++ menus/mainpage.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://menus/buyticketmain.tscn")
